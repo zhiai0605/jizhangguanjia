e256be8b617ea4d37c0d5752e77941bb8eee326c
孙博 <17013587+zay01919@user.noreply.gitee.com>
2026-05-20 13:08:53 +0800

    配置 GitHub Pages 部署

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,15 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths";
 import { traeBadgePlugin } from 'vite-plugin-trae-solo-badge';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/jizhangguanjia/',
   build: {
     sourcemap: 'hidden',
   },
   plugins: [
     react({
       babel: {
-        plugins: [
-          'react-dev-locator',
-        ],
+        plugins: ['react-dev-locator'],
       },
     }),
     traeBadgePlugin({
