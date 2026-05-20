e31ebc4d4121c8f67b6dd8d82a637b5143a6480b
孙博 <17013587+zay01919@user.noreply.gitee.com>
2026-05-20 13:52:40 +0800

    修复 PWA 黑屏问题

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('./sw.js')
   })
 }
 
